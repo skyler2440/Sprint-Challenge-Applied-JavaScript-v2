47c33fef0dc1535a0cc7a087418ac14ccc6274a3
skyler dowdy <skylerwebdev@gmail.com>
2019-07-12 11:38:42 -0400

    Stupid issue appending header
    Moving on getting cannot append property of null will revisit

--- components/Header/index.js
+++ components/Header/index.js
@@ -8,5 +8,27 @@
 //    <span class="temp">98°</span>
 //  </div >
 // And add it to the DOM in the .headerContainer component
+// document.querySelector('header-container').append(header)
 
-function Header() {}
+
+
+function Header() {
+    const header = document.createElement('div'),
+    date = document.createElement('span'),
+    h1 = document.createElement('h1'),
+    temp = document.createElement('span');
+
+    header.append(date);
+    header.append(h1);
+    header.append(temp);
+
+    header.classList.add('header');
+    date.classList.add('date');
+    temp.classList.add('temp');
+
+    date.textContent = 'SMARCH 28, 2019';
+    h1.textContent = 'Lambda Times';
+    temp.textContent = '98°'
+
+    return header
+}
